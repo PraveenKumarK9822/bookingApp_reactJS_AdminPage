3cee8bc4586da9fa516875bde2b1d161bbc8b022
PraveenKumarK9822 <praveensamual@gmail.com>
2025-05-05 22:16:29 +0530

    initial API integrated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,67 @@
-import "./login.scss"
+import React, { useContext, useState } from "react";
+import "./login.scss";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({...prev, [e.target.id]: e.target.value}))
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      if(res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        navigate("/");
+      } else {
+        dispatch({ type: "LOGIN_FAILURE", payload: { message: "You are not allowed to login!" } });
+      }
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  }
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="loginContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="loginInput"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="loginInput"
+          onChange={handleChange}
+        />
+        <button
+          className="loginButton"
+          disabled={loading}
+          onClick={handleClick}
+        >
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
+    </div>
+  );
+};
 
 export default Login
