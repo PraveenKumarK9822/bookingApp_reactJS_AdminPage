032e22185b5b20e0fb1b766d86e7355c14196b45
PraveenKumarK9822 <praveensamual@gmail.com>
2025-05-06 23:37:56 +0530

    login page design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,10 +7,22 @@ import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNone
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+// import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { dispatch } = useContext(DarkModeContext);
+  const { user } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    window.location.reload();
+    // navigate("/Login");
+  };
 
   return (
     <div className="navbar">
@@ -44,15 +56,25 @@ const Navbar = () => {
           <div className="item">
             <ListOutlinedIcon className="icon" />
           </div>
-          <div className="item">
+          <div className="item" onClick={() => setIsOpen(!isOpen)}>
             <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
+              // src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src={user?.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"}
+              alt={user.username}
               className="avatar"
             />
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="dropdown">
+          <div className="dropdownItem">Profile</div>
+          <div className="dropdownItem">Settings</div>
+          <div className="dropdownItem" onClick={handleLogout}>
+            Logout
+          </div>
+        </div>
+      )}
     </div>
   );
 };
